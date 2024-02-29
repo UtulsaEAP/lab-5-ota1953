aaac1a27ae2a54ad79d6ee8214ac4635e1c70333
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-02-29 14:38:15 -0600

    Another Down

--- Problem#1/kilo2lbs.py
+++ Problem#1/kilo2lbs.py
@@ -1,3 +1,4 @@
+#Owen Anderson THUR 2:00-3:15
 def kilo_to_pounds(kilos):
     # This statement intentionally has an error. 
     return (kilos * 2.204)
--- Problem#3/stepCounter.py
+++ Problem#3/stepCounter.py
@@ -1,8 +1,10 @@
 def feet_to_steps(user_feet):
-   #write your code here
+   steps_taken = int(user_feet//2.5)
+   print (steps_taken)
 
 if __name__ == '__main__':
     #take input feet steps here
+    user_feet = float(input())
     #store it into the function
-    
+    feet_to_steps(user_feet)
     #print your steps here
